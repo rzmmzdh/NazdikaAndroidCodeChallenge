581d45283704a3c35764b9df57e137c09ff292e6
Baset <baset.esmaili0@gmail.com>
2023-03-04 23:23:20 +0330

    Implement today_matches api request.

--- app/src/main/java/com/nazdika/code/challenge/MainActivity.kt
+++ app/src/main/java/com/nazdika/code/challenge/MainActivity.kt
@@ -1,11 +1,152 @@
 package com.nazdika.code.challenge
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.GsonBuilder
+import com.google.gson.annotations.SerializedName
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.logging.HttpLoggingInterceptor
+import java.io.IOException
+import java.util.Date
+import java.util.TimeZone
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var androidId: String
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        .build()
+    private lateinit var todayMatchesRequest: Request
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        androidId = Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID)
+        todayMatchesRequest = Request.Builder()
+            .url("https://api.nazdika.com/v2/foot/match/live")
+            .get()
+            .addHeader("X-Seconds-From-UTC", getTimeZoneOffsetFromUTC().toString())
+            .addHeader("X-UDID", androidId)
+            .build()
+        thread(name = "TodayMatchesThread") {
+            okHttpClient.newCall(todayMatchesRequest).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    call.cancel()
+                    runOnUiThread {
+                        Toast.makeText(applicationContext, e.localizedMessage, Toast.LENGTH_LONG)
+                            .show()
+                    }
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    val body = response.body?.string()
+                    val gson = GsonBuilder().create()
+                    val todayMatches = gson.fromJson(body, SoccerMatchesResultPojo::class.java)
+                    onTodayMatchesResponse(todayMatches)
+                }
+
+            })
+        }
     }
+
+    private fun onTodayMatchesResponse(todayMatches: SoccerMatchesResultPojo?) {
+        runOnUiThread {
+            if (todayMatches != null) {
+                if (todayMatches.success) {
+                    showTodayMatches(todayMatches.data?.competitionMatches ?: return@runOnUiThread)
+                } else {
+                    Toast.makeText(this, "Error in load data!", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+    private fun showTodayMatches(competitionMatches: List<CompetitionMatchPojo?>) {
+
+    }
+
+    private fun getTimeZoneOffsetFromUTC(): Int {
+        val tz: TimeZone = TimeZone.getDefault()
+        val now = Date()
+        return tz.getOffset(now.time) / 1000
+    }
+
+    data class SoccerMatchesResultPojo(
+        @SerializedName("success")
+        val success: Boolean,
+        @SerializedName("data")
+        val data: SoccerMatchesResultDataPojo? = null
+    )
+
+    data class SoccerMatchesResultDataPojo(
+        @SerializedName("competition_matches")
+        val competitionMatches: List<CompetitionMatchPojo?>? = null
+    )
+
+    data class CompetitionMatchPojo(
+        @SerializedName("competition_id")
+        val competitionId: Int? = null,
+        @SerializedName("name_fa")
+        val persianName: String? = null,
+        @SerializedName("logo")
+        val logo: String? = null,
+        @SerializedName("localized_name")
+        val localizedName: String? = null,
+        @SerializedName("matches")
+        val matches: List<MatchPojo>? = null
+    )
+
+    data class MatchPojo(
+        @SerializedName("match_id")
+        val matchId: Long? = null,
+        @SerializedName("home_team_score")
+        val homeTeamScore: Int? = null,
+        @SerializedName("away_team_score")
+        val awayTeamScore: Int? = null,
+        @SerializedName("home_team_pen")
+        val homeTeamPen: Int? = null,
+        @SerializedName("away_team_pen")
+        val awayTeamPen: Int? = null,
+        @SerializedName("match_started")
+        val matchStarted: Boolean? = null,
+        @SerializedName("live_url")
+        val liveUrl: String? = null,
+        @SerializedName("live_stream_url")
+        val liveStreamUrl: String? = null,
+        @SerializedName("has_video")
+        val hasVideo: Boolean? = null,
+        @SerializedName("match_ended")
+        val matchEnded: Boolean? = null,
+        @SerializedName("has_live")
+        val hasLive: Boolean? = null,
+        @SerializedName("short_date_text")
+        val shortDateText: String? = null,
+        @SerializedName("has_details")
+        val hasDetails: Boolean? = null,
+        @SerializedName("status")
+        val status: String? = null,
+        @SerializedName("home_team")
+        val homeTeam: TeamPojo? = null,
+        @SerializedName("away_team")
+        val awayTeam: TeamPojo? = null
+    )
+
+    data class TeamPojo(
+        @SerializedName("team_id")
+        val teamId: Long? = null,
+        @SerializedName("name_en")
+        val englishName: String? = null,
+        @SerializedName("name_fa")
+        val persianName: String? = null,
+        @SerializedName("logo")
+        val logo: String? = null,
+        @SerializedName("localized_name")
+        val localizedName: String? = null
+    )
 }
