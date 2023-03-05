dbcbbe3307219ae7b0b63575c798d7ac324b5e0f
rzmmzdh <rzmmzdh@gmail.com>
2023-03-05 18:13:08 +0330

    annotate hilt android app

--- app/src/main/java/com/nazdika/code/challenge/NazdikaLiveScore.kt
+++ app/src/main/java/com/nazdika/code/challenge/NazdikaLiveScore.kt
@@ -2,8 +2,10 @@ package com.nazdika.code.challenge
 
 import android.app.Application
 import com.facebook.drawee.backends.pipeline.Fresco
+import dagger.hilt.android.HiltAndroidApp
 
-class MainApplication : Application() {
+@HiltAndroidApp
+class NazdikaLiveScore : Application() {
     override fun onCreate() {
         super.onCreate()
         Fresco.initialize(this)
