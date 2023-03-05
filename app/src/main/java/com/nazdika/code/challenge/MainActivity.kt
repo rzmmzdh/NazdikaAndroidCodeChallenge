ea618f2987b51cf4efeb923db3bf9a220b7af4a0
Baset <baset.esmaili0@gmail.com>
2023-03-05 08:21:06 +0330

    Implement MainActivity logic and ui

--- app/src/main/java/com/nazdika/code/challenge/MainActivity.kt
+++ app/src/main/java/com/nazdika/code/challenge/MainActivity.kt
@@ -2,19 +2,22 @@ package com.nazdika.code.challenge
 
 import android.os.Bundle
 import android.provider.Settings
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.GsonBuilder
 import com.google.gson.annotations.SerializedName
+import com.nazdika.code.challenge.databinding.ActivityMainBinding
+import java.io.IOException
+import java.util.*
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
-import java.io.IOException
-import java.util.Date
-import java.util.TimeZone
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
@@ -23,10 +26,16 @@ class MainActivity : AppCompatActivity() {
         .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
         .build()
     private lateinit var todayMatchesRequest: Request
+    private lateinit var binding: ActivityMainBinding
+    private val todayMatchesAdapter = TodayMatchesAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.rvTodayMatches.layoutManager =
+            LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        binding.rvTodayMatches.adapter = todayMatchesAdapter
         androidId = Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID)
         todayMatchesRequest = Request.Builder()
             .url("https://api.nazdika.com/v2/foot/match/live")
@@ -39,12 +48,16 @@ class MainActivity : AppCompatActivity() {
                 override fun onFailure(call: Call, e: IOException) {
                     call.cancel()
                     runOnUiThread {
-                        Toast.makeText(applicationContext, e.localizedMessage, Toast.LENGTH_LONG)
+                        binding.progressbar.visibility = View.GONE
+                        Toast.makeText(this@MainActivity, e.localizedMessage, Toast.LENGTH_LONG)
                             .show()
                     }
                 }
 
                 override fun onResponse(call: Call, response: Response) {
+                    runOnUiThread {
+                        binding.progressbar.visibility = View.GONE
+                    }
                     val body = response.body?.string()
                     val gson = GsonBuilder().create()
                     val todayMatches = gson.fromJson(body, SoccerMatchesResultPojo::class.java)
@@ -68,7 +81,50 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showTodayMatches(competitionMatches: List<CompetitionMatchPojo?>) {
-
+        todayMatchesAdapter.addItems(buildList {
+            competitionMatches.forEach { competitionMatch ->
+                add(
+                    CompetitionMatchModel(
+                        competitionMatch?.competitionId,
+                        competitionMatch?.persianName,
+                        competitionMatch?.logo,
+                        competitionMatch?.localizedName
+                    )
+                )
+                addAll(competitionMatch?.matches?.map {
+                    MatchModel(
+                        it.matchId,
+                        it.homeTeamScore,
+                        it.awayTeamScore,
+                        it.homeTeamPen,
+                        it.awayTeamPen,
+                        it.matchStarted,
+                        it.liveUrl,
+                        it.liveStreamUrl,
+                        it.hasVideo,
+                        it.matchEnded,
+                        it.hasLive,
+                        it.shortDateText,
+                        it.hasDetails,
+                        it.status,
+                        TeamModel(
+                            it.homeTeam?.teamId,
+                            it.homeTeam?.englishName,
+                            it.homeTeam?.englishName,
+                            it.homeTeam?.logo,
+                            it.homeTeam?.localizedName
+                        ),
+                        TeamModel(
+                            it.awayTeam?.teamId,
+                            it.awayTeam?.englishName,
+                            it.awayTeam?.englishName,
+                            it.awayTeam?.logo,
+                            it.awayTeam?.localizedName
+                        )
+                    )
+                } ?: return@forEach)
+            }
+        })
     }
 
     private fun getTimeZoneOffsetFromUTC(): Int {
@@ -147,6 +203,46 @@ class MainActivity : AppCompatActivity() {
         @SerializedName("logo")
         val logo: String? = null,
         @SerializedName("localized_name")
+        val localizedName: String? = null
+    )
+
+    interface ItemType {
+        val itemType: Int
+    }
+
+    data class CompetitionMatchModel(
+        val competitionId: Int? = null,
+        val persianName: String? = null,
+        val logo: String? = null,
+        val localizedName: String? = null,
+        override val itemType: Int = 0
+    ) : ItemType
+
+    data class MatchModel(
+        val matchId: Long? = null,
+        val homeTeamScore: Int? = null,
+        val awayTeamScore: Int? = null,
+        val homeTeamPen: Int? = null,
+        val awayTeamPen: Int? = null,
+        val matchStarted: Boolean? = null,
+        val liveUrl: String? = null,
+        val liveStreamUrl: String? = null,
+        val hasVideo: Boolean? = null,
+        val matchEnded: Boolean? = null,
+        val hasLive: Boolean? = null,
+        val shortDateText: String? = null,
+        val hasDetails: Boolean? = null,
+        val status: String? = null,
+        val homeTeam: TeamModel? = null,
+        val awayTeam: TeamModel? = null,
+        override val itemType: Int = 1
+    ) : ItemType
+
+    data class TeamModel(
+        val teamId: Long? = null,
+        val englishName: String? = null,
+        val persianName: String? = null,
+        val logo: String? = null,
         val localizedName: String? = null
     )
 }
---
+++ app/src/main/java/com/nazdika/code/challenge/TodayMatchesAdapter.java
@@ -0,0 +1,76 @@
+package com.nazdika.code.challenge;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.nazdika.code.challenge.databinding.ItemCompetitionBinding;
+import com.nazdika.code.challenge.databinding.ItemMatchBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodayMatchesAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private final List<MainActivity.ItemType> items = new ArrayList<>();
+
+    public void addItems(List<MainActivity.ItemType> items) {
+        this.items.addAll(items);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return items.get(position).getItemType();
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        switch (viewType) {
+            case 0: {
+                return new CompetitionMatchViewHolder(layoutInflater.inflate(R.layout.item_competition, parent, false));
+            }
+            case 1: {
+                return new MatchViewHolder(layoutInflater.inflate(R.layout.item_match, parent, false));
+            }
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (items.get(position).getItemType() == 0) {
+            CompetitionMatchViewHolder viewHolder = (CompetitionMatchViewHolder) holder;
+        } else if (items.get(position).getItemType() == 1) {
+            MatchViewHolder viewHolder = (MatchViewHolder) holder;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class CompetitionMatchViewHolder extends RecyclerView.ViewHolder {
+        final ItemCompetitionBinding binding;
+
+        public CompetitionMatchViewHolder(@NonNull View itemView) {
+            super(itemView);
+            binding = ItemCompetitionBinding.bind(itemView);
+        }
+    }
+
+    public class MatchViewHolder extends RecyclerView.ViewHolder {
+        final ItemMatchBinding binding;
+
+        public MatchViewHolder(@NonNull View itemView) {
+            super(itemView);
+            binding = ItemMatchBinding.bind(itemView);
+        }
+    }
+}
