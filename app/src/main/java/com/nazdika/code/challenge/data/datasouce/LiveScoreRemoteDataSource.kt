e83aa284e51319b19e6f97a41e9c2c86cec817b5
rzmmzdh <rzmmzdh@gmail.com>
2023-03-05 18:16:08 +0330

    add live score remote data source

---
+++ app/src/main/java/com/nazdika/code/challenge/data/datasouce/LiveScoreRemoteDataSource.kt
@@ -0,0 +1,12 @@
+package com.nazdika.code.challenge.data.datasouce
+
+import com.nazdika.code.challenge.data.datasouce.remote.NazdikaApi
+import com.nazdika.code.challenge.model.dto.SoccerMatchesResultDto
+import retrofit2.Response
+import javax.inject.Inject
+
+class LiveScoreRemoteDataSource @Inject constructor(private val api: NazdikaApi) {
+    suspend fun getTodayMatches(): Response<SoccerMatchesResultDto> {
+        return api.getTodayMatches()
+    }
+}
