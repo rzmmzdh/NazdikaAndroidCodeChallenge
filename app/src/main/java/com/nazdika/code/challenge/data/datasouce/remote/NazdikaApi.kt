f4b8352dd2475ea5a20594acbd7df1f1766f0d33
rzmmzdh <rzmmzdh@gmail.com>
2023-03-05 18:17:03 +0330

    add nazdika api service

---
+++ app/src/main/java/com/nazdika/code/challenge/data/datasouce/remote/NazdikaApi.kt
@@ -0,0 +1,10 @@
+package com.nazdika.code.challenge.data.datasouce.remote
+
+import com.nazdika.code.challenge.model.dto.SoccerMatchesResultDto
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface NazdikaApi {
+    @GET("foot/match/live")
+    suspend fun getTodayMatches(): Response<SoccerMatchesResultDto>
+}
