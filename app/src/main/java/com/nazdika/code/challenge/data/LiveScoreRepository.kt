25f2643e0e17d17f6fdaa6372280f2fbe559bbce
rzmmzdh <rzmmzdh@gmail.com>
2023-03-05 18:15:52 +0330

    add live score repository

---
+++ app/src/main/java/com/nazdika/code/challenge/data/LiveScoreRepository.kt
@@ -0,0 +1,41 @@
+package com.nazdika.code.challenge.data
+
+import com.nazdika.code.challenge.data.datasouce.LiveScoreRemoteDataSource
+import com.nazdika.code.challenge.data.datasouce.Result
+import com.nazdika.code.challenge.model.CompetitionMatch
+import com.nazdika.code.challenge.model.dto.CompetitionMatchDto
+import com.nazdika.code.challenge.model.dto.asCompetitionMatch
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class LiveScoreRepository @Inject constructor(private val liveScoreRemoteDataSource: LiveScoreRemoteDataSource) {
+    suspend fun getTodayMatches(): Flow<Result<List<CompetitionMatch?>?>> =
+        flow {
+            val result = liveScoreRemoteDataSource.getTodayMatches()
+            emit(Result.Loading)
+            when {
+                result.isSuccessful -> {
+                    emit(
+                        Result.Success(
+                            data = result.body()?.data?.competitionMatches?.map {
+                                it?.asCompetitionMatch()
+                                    ?: CompetitionMatch()
+                            }
+                                ?: emptyList()
+                        )
+                    )
+                }
+
+                else -> {
+                    emit(
+                        Result.Error(
+                            exception = Throwable(
+                                message = "An error occurred: ${result.code()}"
+                            )
+                        )
+                    )
+                }
+            }
+        }
+}
