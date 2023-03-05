e73440ad154cb0df1e3b4a26927ac58d9ab90e04
rzmmzdh <rzmmzdh@gmail.com>
2023-03-05 18:11:53 +0330

    remove redundant model

--- app/src/main/java/com/nazdika/code/challenge/model/SoccerMatchDataPojo.kt
+++
@@ -1,8 +0,0 @@
-package com.nazdika.code.challenge.model
-
-import com.google.gson.annotations.SerializedName
-
-data class SoccerMatchesResultDataPojo(
-    @SerializedName("competition_matches")
-    val competitionMatches: List<CompetitionMatchPojo?>? = null
-)
