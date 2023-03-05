0da65910bc3e629587cf37545387c822adf98fed
rzmmzdh <rzmmzdh@gmail.com>
2023-03-05 18:17:17 +0330

    implement dto covnerter

---
+++ app/src/main/java/com/nazdika/code/challenge/model/dto/CompetitionMatchDto.kt
@@ -0,0 +1,26 @@
+package com.nazdika.code.challenge.model.dto
+
+import com.google.gson.annotations.SerializedName
+import com.nazdika.code.challenge.model.CompetitionMatch
+
+data class CompetitionMatchDto(
+    @SerializedName("competition_id")
+    val competitionId: Int? = null,
+    @SerializedName("name_fa")
+    val persianName: String? = null,
+    @SerializedName("logo")
+    val logo: String? = null,
+    @SerializedName("localized_name")
+    val localizedName: String? = null,
+    @SerializedName("matches")
+    val matches: List<MatchDto>? = null
+)
+
+fun CompetitionMatchDto.asCompetitionMatch() =
+    CompetitionMatch(
+        competitionId = competitionId,
+        persianName = persianName,
+        logo = logo,
+        localizedName = localizedName,
+        matches = matches?.map { it.asMatch() }
+    )
