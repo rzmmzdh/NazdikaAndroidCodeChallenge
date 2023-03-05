72450ef319c116a1e2b76f6b5c56dcced7c68b8d
rzmmzdh <rzmmzdh@gmail.com>
2023-03-05 10:21:17 +0330

    refactor team model

---
+++ app/src/main/java/com/nazdika/code/challenge/model/TeamModel.kt
@@ -0,0 +1,9 @@
+package com.nazdika.code.challenge.model
+
+data class TeamModel(
+    val teamId: Long? = null,
+    val englishName: String? = null,
+    val persianName: String? = null,
+    val logo: String? = null,
+    val localizedName: String? = null
+)
